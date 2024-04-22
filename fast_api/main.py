71a882c4fb95775c9c99b3be3d523c1437b8b5d1
Sandalcho7 <bahurel.anatole@gmail.com>
2024-04-22 13:51:51 +0200

    debugging

--- fast_api/main.py
+++ fast_api/main.py
@@ -8,7 +8,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from scraper import scrape_data
 
 
-HEADERS = os.getenv("EDEN_AI_KEY")
+HEADERS = os.getenv("EDEN_KEY")
 URL = "https://api.edenai.run/v2/text/chat"
 PROVIDER = "mistral"
 SCRAPED_URL = "http://abb09.westeurope.azurecontainer.io:8001/"
