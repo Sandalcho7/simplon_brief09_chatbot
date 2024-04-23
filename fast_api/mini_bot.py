6803bc9617f36b75c4dd7822a791cfebf40f22f8
Sandalcho7 <bahurel.anatole@gmail.com>
2024-04-23 11:32:36 +0200

    readme update, config in new file

--- fast_api/mini_bot.py
+++ fast_api/mini_bot.py
@@ -1,37 +1,26 @@
 import json
 import requests
 
-from keys.key import key
+from config import HEADERS, TOOL_URL, PROVIDER, payload
 
-HEADERS = {"Authorization": f"Bearer {key}"}
-URL = "https://api.edenai.run/v2/text/chat"
-PROVIDER = 'mistral'
-
-payload = {
-        "providers": PROVIDER,
-        "text": "",
-        "chatbot_global_action": "Act as Queen Elisabeth and answer in less than 100 words",
-        "previous_history": [],
-        "temperature": 0.0,
-        "max_tokens": 150,
-        "fallback_providers": ""
-    }
 
 def chatbot():
     while True:
-        prompt = input('Q: ')
+        prompt = input("Q: ")
         payload["text"] = prompt
-    
-        if prompt in ['quit', 'logout', 'leave', 'exit', 'bye', 'stop']:
+
+        if prompt in ["quit", "logout", "leave", "exit", "bye", "stop"]:
             break
-        
-        response = requests.post(URL, json=payload, headers=HEADERS)
+
+        response = requests.post(TOOL_URL, json=payload, headers=HEADERS)
         result = json.loads(response.text)
         rp = result[PROVIDER]
-    
+
         formatted_response = "\nR: " + rp["generated_text"] + "\n"
-    
+
         payload["previous_history"].append({"role": "user", "message": prompt})
-        payload["previous_history"].append({"role": "assistant", "message": rp["generated_text"]})
-        
+        payload["previous_history"].append(
+            {"role": "assistant", "message": rp["generated_text"]}
+        )
+
         print(formatted_response)
